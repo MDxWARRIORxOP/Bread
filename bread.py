61ceedb100f4aa6b0ce7bc498275243874073612
kingerious <mohammedsiddiqui2009@gmail.com>
2022-08-14 14:01:51 +0530

    THE AWESOME BREAD GAME

---
+++ bread.py
@@ -0,0 +1,30 @@
+# compiled using nuitka
+# bread.dist/bread.exe == main file
+
+def start():
+    print("Welcome to the BREAD")
+    print("as we all know, Bread is awesome and very cool")
+    print("So i decided to create a game about it!")
+    print("this game will let you bake bread in different forms!")
+    print("So lets bake some bread!")
+
+def bake():
+    while(True):
+        print("Bake a bread!")
+        type = input("Which form do you want to bake your bread in? (q to quit)> ")
+        
+        if type == "q" or type == "quit":
+            print("Dont forget Bread is awesome! Bye!")
+            break
+        
+        if type.startswith("a") or type.startswith("e") or type.startswith("i") or type.startswith("o") or type.startswith("u"):
+            aOrAn = "an"
+        else:
+            aOrAn = "a"
+            
+        print(f"You baked {aOrAn} {type}")
+    quit()
+
+if __name__=="__main__":
+    start()
+    bake()
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Awesome Bread game
+
+This is an awesome bread game built By Kingerious for the Bread organization (https://discord.gg/dKaptM4Pgm)
+
+
+## running the awesome bread game
+
+after cloning this repo,
+
+just run:
+```bash
+python bread.py
+```
