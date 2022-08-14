e35a1c416442d2301c8568ce8f3f9d9296cd061c
kingerious <mohammedsiddiqui2009@gmail.com>
2022-08-14 14:14:08 +0530

    much better

--- bread.py
+++ bread.py
@@ -1,12 +1,17 @@
-# compiled using nuitka
-# bread.dist/bread.exe == main file
+import time
 
 def start():
+    timeToSleep = 1
     print("Welcome to the BREAD")
+    time.sleep(timeToSleep)
     print("as we all know, Bread is awesome and very cool")
+    time.sleep(timeToSleep)
     print("So i decided to create a game about it!")
+    time.sleep(timeToSleep)
     print("this game will let you bake bread in different forms!")
+    time.sleep(timeToSleep)
     print("So lets bake some bread!")
+    time.sleep(timeToSleep)
 
 def bake():
     while(True):
